cc9656f8f1b68277c7d38bf2f361e5b43cc1a5bf
Michael Davis <mcd164@miami.edu>
2018-08-18 18:59:33 -0400

    Add the shell for a mix task

---
+++ lib/mix/tasks/spectre.ex
@@ -0,0 +1,29 @@
+defmodule Mix.Tasks.Spectre do
+  use Mix.Task
+
+  @shortdoc "Generate specs on functions"
+  @moduledoc """
+  Spectre creates `@spec`s on functions that don't have any. Spectre
+  does not perform any static type checking, and will try to spec every
+  function even the code will create run-time errors.
+
+  ## Usage
+
+  In any project's root directory
+
+      mix spectre
+
+  Recommended usage:
+
+  1. Commit all your changes in your project directory.
+  2. `mix spectre` in the project root.
+  3. Use `git diff` to view the new specs.
+  """
+  @preferred_cli_env :test
+  @recursive false
+
+  @impl Mix.Task
+  def run(argv) do
+    argv
+  end
+end
